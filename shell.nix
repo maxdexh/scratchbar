ecfa86ab9ede1f7ef0635b53cc160071db51d3f8
Max Dexheimer <maxdexh03@gmail.com>
2026-02-22 12:43:30 +0100

    Add MSRV

--- shell.nix
+++ shell.nix
@@ -1,11 +1,14 @@
+# TODO: Use a flake with flake-compat, add rust toolchain once 1.93 is in nixpkgs
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   nativeBuildInputs = [pkgs.pkg-config];
 
   buildInputs = [
     pkgs.rustPlatform.bindgenHook
-    pkgs.libpulseaudio
-    pkgs.pulseaudio
     pkgs.wlr-randr
+
+    # For the example controller
+    pkgs.pulseaudio
+    pkgs.libpulseaudio
   ];
 }
